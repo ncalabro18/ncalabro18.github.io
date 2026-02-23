3674bd6499b2a5706f59d6a7e76ca3947c1710c7
ncalabro18 <nicholasjcalabro@gmail.com>
2026-02-23 18:29:35 +0000

    add check h1 and h3 content tests

--- src/app/about/about.spec.ts
+++ src/app/about/about.spec.ts
@@ -1,23 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { About } from './about';
+import { AboutPage } from './about';
 
 describe('About', () => {
-  let component: About;
-  let fixture: ComponentFixture<About>;
+  let component: AboutPage;
+  let fixture: ComponentFixture<AboutPage>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [About]
+      imports: [AboutPage]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(About);
+    fixture = TestBed.createComponent(AboutPage);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render h1', async () => {
+    const fixture = TestBed.createComponent(AboutPage);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Nicholas J. Calabro');
+  });
+
+  it('should render h2', async () => {
+    const fixture = TestBed.createComponent(AboutPage);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h3')?.textContent).toContain('University of Massachusetts, Lowell');
   });
 });
